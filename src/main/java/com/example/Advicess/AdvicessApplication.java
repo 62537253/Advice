96801942a1a1b0933c6101d2acc6585dd6dd3ebd
62537253 <144789858+62537253@users.noreply.github.com>
2023-12-22 17:27:03 +0700

    First commit

---
+++ src/main/java/com/example/Advicess/AdvicessApplication.java
@@ -0,0 +1,13 @@
+package com.example.Advicess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvicessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvicessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Advicess/AdvicessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Advicess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvicessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
